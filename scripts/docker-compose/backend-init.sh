798680dca2c59a3f788aac45588a1d1291ce03f1
Retspen <anatoliy.guskov@gmail.com>
2022-11-12 12:33:23 +0200

    Fixed fixed mode

--- scripts/docker-compose/backend-init.sh
+++ scripts/docker-compose/backend-init.sh
@@ -5,3 +5,4 @@ docker-compose -f compose-dev-backend.yml up -d
 sleep 5
 docker-compose -f compose-dev-backend.yml exec mariadb mysql -uroot -proot -e "CREATE DATABASE webvirtcloud CHARACTER SET utf8 COLLATE utf8_general_ci"
 docker-compose -f compose-dev-backend.yml exec backend python3 manage.py migrate
+docker-compose -f compose-dev-backend.yml exec backend bash -c "python3 manage.py loaddata */fixtures/*.json"
